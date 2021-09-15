1fdbd9ea397fd77fb0806362816b00258f5e6013
Matias Suez <matisuez@gmail.com>
2021-09-14 23:40:55 -0300

    Database setup - RocksDB

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -7,21 +7,13 @@ use std::error::Error;
 use serde_derive::{ Serialize };
 use warp::http::StatusCode;
 use warp::{ Filter, Rejection, Reply };
+use rocksdb::{ DB };
 
-/// Rejections represent cases where a filter should not continue processing
-/// the request, but a different filter *could* process it.
 #[tokio::main]
 async fn main() {
 
-    // let db = DB::open_default("path/for/rocksdb/storage").unwrap();
-    // db.put(b"my key", b"my value");
-    // match db.get(b"my key") {
-    //    Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
-    //    Ok(None) => println!("value not found"),
-    //    Err(e) => println!("operational problem encountered: {}", e),
-    // }
-    // db.delete(b"my key").unwrap();
-
+    let _db = DB::open_default("path/for/rocksdb/storage").unwrap();
+    
     // GET /
     let index = warp::path::end()
         .map(|| { 
@@ -59,22 +51,18 @@ async fn main() {
 
 }
 
-/// An API error serializable to JSON.
 #[derive(Serialize)]
 struct ResponseMessage {
     code: u16,
     message: String,
 }
 
-/// An API error serializable to JSON.
 #[derive(Serialize)]
 struct ErrorMessage {
     code: u16,
     message: String,
 }
 
-// This function receives a `Rejection` and tries to return a custom
-// value, otherwise simply passes the rejection along.
 async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
     let code;
     let message;
@@ -83,8 +71,7 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
         code = StatusCode::NOT_FOUND;
         message = "Page not found.";
     } else if let Some(e) = err.find::<warp::filters::body::BodyDeserializeError>() {
-        // This error happens if the body could not be deserialized correctly
-        // We can use the cause to analyze the error and customize the error message
+
         message = match e.source() {
             Some(cause) => {
                 if cause.to_string().contains("denom") {
@@ -97,12 +84,9 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
         };
         code = StatusCode::BAD_REQUEST;
     } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
-        // We can handle a specific error, here METHOD_NOT_ALLOWED,
-        // and render it however we want
         code = StatusCode::METHOD_NOT_ALLOWED;
         message = "METHOD_NOT_ALLOWED";
     } else {
-        // We should have expected this... Just log and say its a 500
         eprintln!("unhandled rejection: {:?}", err);
         code = StatusCode::INTERNAL_SERVER_ERROR;
         message = "UNHANDLED_REJECTION";
@@ -114,4 +98,5 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
     });
 
     Ok(warp::reply::with_status(json, code))
+
 }
